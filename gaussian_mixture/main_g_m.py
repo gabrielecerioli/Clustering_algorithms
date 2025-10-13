1ecb635e6c3691e50a42535bb359e467dbb921b5
Gabriele Cerioli <gabrielecerioli120104@gmail.com>
2025-10-13 21:26:35 +0200

    added notebooks

---
+++ gaussian_mixture/main_g_m.py
@@ -0,0 +1,1 @@
+# Now instead of identifying clusters by "nearest" centroids, we fit a set of k gaussians to the data, and we estimate the parameters  (mean and variance) for eanch cluster and weight of a cluster.
